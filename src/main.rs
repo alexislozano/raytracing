55eaeed0080d0ab6d5467649f73a62350272b77c
Alexis Lozano <alexis.pascal.lozano@gmail.com>
2019-07-20 20:26:43 +0200

    Chapter 12

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use vec3::Vec3;
 
 use rand::prelude::*;
 
-fn color(r: &Ray, world: &HitableList, depth: u32) -> Vec3 {
+fn color(r: &Ray, world: &Hitable, depth: u32) -> Vec3 {
     let (hit_record, material) = world.hit(r, 0.001, std::f64::INFINITY);
     match hit_record {
         Some(hit_record) => {
@@ -39,8 +39,71 @@ fn color(r: &Ray, world: &HitableList, depth: u32) -> Vec3 {
     }
 }
 
+fn random_scene() -> HitableList {
+    let mut rng = rand::thread_rng();
+    let mut list: Vec<Box<Hitable>> = vec![];
+    list.push(Box::new(Sphere::new(
+        Vec3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        Material::Lambertian { attenuation: Vec3::new(0.5, 0.5, 0.5) }
+    )));
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = rng.gen::<f64>();
+            let center = Vec3::new(a as f64 + 0.9 * rng.gen::<f64>(), 0.2, b as f64 + 0.9 * rng.gen::<f64>());
+            if (center - Vec3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                if choose_mat < 0.8 {
+                    list.push(Box::new(Sphere::new(
+                        center,
+                        0.2,
+                        Material::Lambertian { attenuation: Vec3::new(
+                            rng.gen::<f64>() * rng.gen::<f64>(),
+                            rng.gen::<f64>() * rng.gen::<f64>(),
+                            rng.gen::<f64>() * rng.gen::<f64>(), 
+                        )}
+                    )));
+                } else if choose_mat < 0.95 {
+                    list.push(Box::new(Sphere::new(
+                        center,
+                        0.2,
+                        Material::Metal { attenuation: Vec3::new(
+                            0.5 * (1.0 + rng.gen::<f64>()),
+                            0.5 * (1.0 + rng.gen::<f64>()),
+                            0.5 * (1.0 + rng.gen::<f64>()), 
+                        ), fuzziness: 0.5 * rng.gen::<f64>() }
+                    )));
+                } else {
+                    list.push(Box::new(Sphere::new(
+                        center,
+                        0.2,
+                        Material::Dielectric { refraction: 1.5 }
+                    )));
+                }
+            } 
+        }
+    }
+    list.push(Box::new(Sphere::new(
+        Vec3::new(0.0, 1.0, 0.0),
+        1.0,
+        Material::Dielectric { refraction: 1.5 }
+    )));
+    list.push(Box::new(Sphere::new(
+        Vec3::new(-4.0, 1.0, 0.0),
+        1.0,
+        Material::Lambertian { attenuation: Vec3::new(0.4, 0.2, 0.1) }
+    )));
+    list.push(Box::new(Sphere::new(
+        Vec3::new(4.0, 1.0, 0.0),
+        1.0,
+        Material::Metal { attenuation: Vec3::new(0.7, 0.6, 0.5), fuzziness: 0.0 }
+    )));
+    HitableList::new(list)
+}
+
 fn main() {
     let mut rng = rand::thread_rng();
+
+    let world = random_scene();
 
     let width = 200;
     let height = 100;
@@ -49,47 +112,16 @@ fn main() {
 
     let mut pic = format!("P3\n{} {}\n{}\n", width, height, max_color);
 
-    let material1 = Material::Lambertian {
-        attenuation: Vec3::new(0.1, 0.2, 0.5),
-    };
-    let material2 = Material::Lambertian {
-        attenuation: Vec3::new(0.8, 0.8, 0.0),
-    };
-    let material3 = Material::Metal {
-        attenuation: Vec3::new(0.8, 0.6, 0.2),
-        fuzziness: 0.3,
-    };
-    let material4 = Material::Dielectric {
-        refraction: 1.5,
-    };
-    let material5 = Material::Dielectric {
-        refraction: 1.5,
-    };
-
-    let hitable1 = Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5, material1);
-    let hitable2 = Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0, material2);
-    let hitable3 = Sphere::new(Vec3::new(1.0, 0.0, -1.0), 0.5, material3);
-    let hitable4 = Sphere::new(Vec3::new(-1.0, 0.0, -1.0), 0.5, material4);
-    let hitable5 = Sphere::new(Vec3::new(-1.0, 0.0, -1.0), -0.45, material5);
-
-    let world = HitableList::new(vec![
-        Box::new(hitable1),
-        Box::new(hitable2),
-        Box::new(hitable3),
-        Box::new(hitable4),
-        Box::new(hitable5),
-    ]);
-
-    let lookfrom = Vec3::new(3.0, 3.0, 2.0);
-    let lookat = Vec3::new(0.0, 0.0, -1.0);
+    let lookfrom = Vec3::new(16.0, 2.0, 4.0);
+    let lookat = Vec3::new(0.0, 0.0, 0.0);
     let dist_to_focus = (lookfrom - lookat).length();
-    let aperture = 2.0;
+    let aperture = 0.2;
 
     let camera = Camera::new(
         lookfrom,
         lookat,
         Vec3::new(0.0, 1.0, 0.0),
-        20.0, 
+        15.0, 
         width as f64 / height as f64,
         aperture,
         dist_to_focus,
